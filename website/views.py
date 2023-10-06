682005eb2871b74996b167882427cc261446a52c
root <root@ip-172-31-20-94.eu-north-1.compute.internal>
2023-10-06 09:17:02 +0000

    remove unneeded functions

--- website/views.py
+++ website/views.py
@@ -58,28 +58,31 @@ def register_user(request):
 
 
 # show user (from auth)
+"""
 def customer_record(request, pk):
     if request.user.is_authenticated:
         # Look Up Records
         customer_record = Record.objects.get(id=pk)
-        return render(request, 'record.html'""", {'customer_record':customer_record}""")
+        return render(request, 'record.html', {'customer_record':customer_record})
     else:
         messages.success(request, "You Must Be Logged In To View That Page...")
         return redirect('home')
-
+"""
 
 # deletion of user (via toolbar)
+"""
 def delete_record(request, pk):
     if request.user.is_authenticated:
-        """delete_it = Record.objects.get(id=pk)
-        delete_it.delete()"""
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
         messages.success(request, "Record Deleted Successfully...")
         return redirect('home')
     else:
         messages.success(request, "You Must Be Logged In To Do That...")
-        return redirect('home')
-
-"""def add_record(request):
+        return redirect('home') 
+"""    
+"""
+def add_record(request):
     form = AddRecordForm(request.POST or None)
     if request.user.is_authenticated:
         if request.method == "POST":
@@ -105,5 +108,5 @@ def update_record(request, pk):
         return render(request, 'update_record.html', {'form':form})
     else:
         messages.success(request, "You Must Be Logged In...")
-        return redirect('home')
-""" 
+        return redirect('home') 
+ """
